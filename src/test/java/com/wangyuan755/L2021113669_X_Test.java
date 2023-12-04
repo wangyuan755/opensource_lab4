b8073fd7ea342bb31cf26a0e5313672eba65b07b
wangyuan755 <64016486+wangyuan755@users.noreply.github.com>
2023-12-05 00:32:32 +0800

    Add files via upload

---
+++ src/test/java/com/wangyuan755/L2021113669_X_Test.java
@@ -0,0 +1,20 @@
+package com.wangyuan755;
+
+
+import java.beans.Transient;
+import java.util.*;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class L2021113669_X_Test extends Solution20
+{
+    @Test
+    public void testfindMinHeightTrees()
+    {
+        List<Integer> a = new ArrayList<Integer>();
+        a.add(1);
+        Assert.assertEquals(a, findMinHeightTrees(4, new int[][]{{1,0},{1,2},{1,3}})); 
+          
+    }   
+}
